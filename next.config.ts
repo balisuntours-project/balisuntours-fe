148619541c60ac0e1fca97d2d9c1f5a635a45a81
rizkysix <rizkysix666@gmail.com>
2024-11-14 01:47:19 +0800

    fix: off eslint temporary

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+},
   images: {
     domains: ['booking.balisuntours.com', 'source.unsplash.com', '127.0.0.1'],
   }
--- src/app/enum/activity.enum.ts
+++ src/app/enum/activity.enum.ts
@@ -1,17 +1,16 @@
 export enum ActivityStatusEnum {
-    unpublished = "Unpublished",
-    published = "Published",
-    
+  unpublished = "Unpublished",
+  published = "Published",
 }
 
 export enum ActivityPackageTypeEnum {
-    basicItinerary = "1",
-    pickupTimeByTeam = "2",
-    pickupTimeByTraveller = "3",
-    freeTour = "4"
+  basicItinerary = "1",
+  pickupTimeByTeam = "2",
+  pickupTimeByTraveller = "3",
+  freeTour = "4",
 }
 
 export enum ActivityPackageSelfConfirmationStatus {
-    self = "1",
-    waiting = "0"
+  self = "1",
+  waiting = "0",
 }
